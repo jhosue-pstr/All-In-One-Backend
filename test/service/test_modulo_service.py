5892d472e9f697179b1688a4ff31af83344b9a0e
jhosue-pstr <pastor.jhosue07@gmail.com>
2026-04-25 17:00:49 -0500

    subiendo coverage 4

--- test/service/test_modulo_service.py
+++ test/service/test_modulo_service.py
@@ -1,10 +1,14 @@
+import pytest
+from app.service.modulo import create_modulo
+from app.schemas.modulo import ModuloCreate
+from sqlalchemy.exc import IntegrityError
+
+
 def test_create_modulo_duplicate_error(db):
-    from app.service.modulo import create_modulo
-    from app.schemas.modulo import ModuloCreate
-    from sqlalchemy.exc import SQLAlchemyError
-    
-    data = ModuloCreate(nombre="Test", slug="dup-test", tipo="test")
-    create_modulo(db, data)
-    
-    with pytest.raises(SQLAlchemyError):
-        create_modulo(db, data)
+    from app.models.modulo import Modulo
+
+    db.add(Modulo(nombre="Test", slug="dup-test-svc", tipo="x"))
+    db.commit()
+
+    with pytest.raises(IntegrityError):
+        create_modulo(db, ModuloCreate(nombre="Test2", slug="dup-test-svc", tipo="x"))
