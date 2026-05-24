06daf9841e822cc4682c534af288f6716805d31b
jhosue-pstr <pastor.jhosue07@gmail.com>
2026-05-24 13:47:55 -0500

    agregnaod pipeline

--- test/service/test_integracion_modulos.py
+++ test/service/test_integracion_modulos.py
@@ -1,5 +1,5 @@
 import pytest
-from httpx import AsyncClient, ASGITransport # <-- Importar ASGITransport
+from httpx import AsyncClient, ASGITransport
 from app.main import app
 
 @pytest.mark.asyncio
@@ -10,13 +10,14 @@ async def test_integracion_modulos():
             "correo": "test@test.com", "contrasena": "test123",
             "nombre": "Test", "apellido": "User"
         })
+
         resp_login = await ac.post("/api/auth/inicio", data={
             "username": "test@test.com", "password": "test123"
         })
         token = resp_login.json()["access_token"]
         headers = {"Authorization": f"Bearer {token}"}
 
-        await ac.post("/api/sitios", json={"nombre": "Test Sitio", "slug": "test-sitio"}, headers=headers)
+        await ac.post("/api/sitios/", json={"nombre": "Test Sitio", "slug": "test-sitio"}, headers=headers)
 
         resp_blog = await ac.get("/modules/blog/1/posts")
         assert resp_blog.status_code == 200
