ebbc05310105d6960f7b07029573c82208d2050d
jhosue-pstr <pastor.jhosue07@gmail.com>
2026-06-23 14:05:08 -0500

    fix: agregar localhost:5173 a allow_origins de CORS y cambiar allow_credentials=True

--- app/main.py
+++ app/main.py
@@ -61,8 +61,12 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
